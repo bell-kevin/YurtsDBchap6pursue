9929fc177275a89e4fadc11957d63f9ed2a8f1de
Kevin Bell <bellKevin@pm.me>
2022-12-06 12:58:54 -0700

    Update insertRoom.sql

--- insertRoom.sql
+++ insertRoom.sql
@@ -1,2 +1,3 @@
 INSERT INTO room(room_number, beds, max_occupants, smoking_room, pet_room, nightly_cost) VALUES (1,1,2,0,0,200.00);
 INSERT INTO room(room_number, beds, max_occupants, smoking_room, pet_room, nightly_cost) VALUES (2,2,2,1,1,201.00);
+INSERT INTO room(room_number, beds, max_occupants, smoking_room, pet_room, nightly_cost) VALUES (3,3,3,0,0,202.02);
