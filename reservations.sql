d67e6db3ba56a4612af8d2b80c55352d6683c8d3
Kevin Bell <bellKevin@pm.me>
2022-12-06 13:04:54 -0700

    Create reservations.sql

---
+++ reservations.sql
@@ -0,0 +1,1 @@
+INSERT INTO reservations(reservation_id, customer_id, check_in_date, check_out_date, reservation_date, room_number) VALUES (1,100,'20221207 10:34:09 AM','20230101 10:34:09 AM','20221206 10:34:09 AM',1);
