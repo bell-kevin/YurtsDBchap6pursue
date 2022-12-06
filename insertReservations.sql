034ec20a9592985410f28be7f4cc807028f0ba56
Kevin Bell <bellKevin@pm.me>
2022-12-06 13:18:09 -0700

    Update insertReservations.sql

--- insertReservations.sql
+++ insertReservations.sql
@@ -1,2 +1,3 @@
 INSERT INTO reservations(reservation_id, customer_id, check_in_date, check_out_date, reservation_date, room_number) VALUES (1,100,'20221207 10:34:09 AM','20230101 10:34:09 AM','20221206 10:34:09 AM',1);
 INSERT INTO reservations(reservation_id, customer_id, check_in_date, check_out_date, reservation_date, room_number) VALUES (2,101,'20221209 10:34:09 AM','20221216 10:34:09 AM','20221205 10:34:09 AM',2);
+INSERT INTO reservations(reservation_id, customer_id, check_in_date, check_out_date, reservation_date, room_number) VALUES (3,102,'20221210 10:34:09 AM','20221219 10:34:09 AM','20221204 10:34:09 AM',3);
